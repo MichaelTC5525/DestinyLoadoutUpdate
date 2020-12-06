88ecaf803629c1da14740a28b17940ebf7a7ab0a
Michael Cheung <michael.cheung@live.ca>
2020-12-06 11:32:41 -0800

    Directory setup; preliminary design

---
+++ src/main/util/ArgReader.java
@@ -0,0 +1,20 @@
+package main.util;
+
+public class ArgReader {
+
+    private String accountName;
+    private String guardianClass;
+    private int maxKinetic;
+    private int maxEnergy;
+    private int maxHeavy;
+    private int maxHelmet;
+    private int maxGauntlet;
+    private int maxChest;
+    private int maxLeg;
+    private int maxClassItem;
+
+    public ArgReader() {
+
+    }
+
+}
---
+++ src/test/util/ArgReaderTest.java
@@ -0,0 +1,17 @@
+package test.util;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ArgReaderTest {
+
+    @BeforeEach
+    public void setup() {
+
+    }
+
+    @Test
+    public void testConstructor() {
+
+    }
+}
